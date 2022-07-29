ec7b23ab9164f8222b2d9acae783c0445b9ea530
Ridhima Jain <ridhimajain65@gmail.com>
2022-07-30 01:37:35 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  images:{
+    domains: ["links.papareact.com", "image.tmdb.org"],
+},
+
 }
 
 module.exports = nextConfig
